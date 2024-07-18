2564913c44c359725b8fb2dbe8b266c7e020c1e4
Yasiru Lakintha <96948952+YasiruLaki@users.noreply.github.com>
2024-07-18 14:09:01 +0530

    dashboard responsive

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -13,6 +13,7 @@ function Dashboard() {
     const [loading, setLoading] = useState(false);
     const [coursesCount, setCoursesCount] = useState(0);
     const [submissionsCount, setSubmissionsCount] = useState(0);
+    const [announcements, setAnnouncements] = useState([]);
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -44,10 +45,10 @@ function Dashboard() {
         fetchUserData();
     }, [currentUser]);
 
+    // Fetch upcoming classes
     useEffect(() => {
         const fetchUpcomingClasses = async () => {
             if (userData && userData.courses) {
-                setLoading(true);
                 try {
                     const classesRef = collection(firestore, 'classes');
                     const q = query(classesRef, where('course', 'in', userData.courses));
@@ -56,40 +57,86 @@ function Dashboard() {
                     querySnapshot.forEach(doc => {
                         const data = doc.data();
                         data.id = doc.id;
-
                         if (data.time && data.time.seconds) {
                             data.time = new Date(data.time.seconds * 1000 + data.time.nanoseconds / 1000000);
                         } else {
                             console.warn('Invalid time format:', data.time);
                         }
-
                         classes.push(data);
                     });
-
-                    console.log('Fetched classes:', classes);
                     const now = new Date();
                     const upcomingClasses = classes
                         .filter(c => c.time > now)
                         .sort((a, b) => a.time - b.time);
-
-                    console.log('Filtered upcoming classes:', upcomingClasses);
-
-                    setUpcomingClass(upcomingClasses[0] || null);
+                    setUpcomingClass(upcomingClasses);
                 } catch (error) {
                     console.error('Error fetching upcoming classes:', error);
                     setError('Failed to fetch upcoming classes. Please try again.');
-                } finally {
+                }
+                finally {
                     setLoading(false);
                 }
             }
+
         };
 
-        fetchUpcomingClasses();
+
+        if (userData) {
+            fetchUpcomingClasses();
+
+        }
+
     }, [userData]);
 
+    // Fetch announcements
+    const fetchAnnouncements = async () => {
+        try {
+            const announcementsRef = collection(firestore, 'announcements');
+            const querySnapshot = await getDocs(announcementsRef);
+            const fetchedAnnouncements = [];
+            querySnapshot.forEach(doc => {
+                const data = doc.data();
+                data.id = doc.id;
+                if (data.date && data.date.toDate) {
+                    data.date = data.date.toDate(); // Convert Firestore Timestamp to JavaScript Date
+                } else {
+                    console.warn('Date format is not a Firestore Timestamp or is missing:', data.date);
+                    data.date = new Date(); // Fallback to current date if date is missing
+                }
+                fetchedAnnouncements.push(data);
+            });
+            setAnnouncements(fetchedAnnouncements);
+        } catch (error) {
+            console.error('Error fetching announcements:', error);
+            setError('Failed to fetch announcements. Please try again.');
+        }
+    };
+
     useEffect(() => {
-        console.log('upcomingClass state:', upcomingClass);
-    }, [upcomingClass]);
+        fetchAnnouncements();
+
+    }, []);
+
+    const handleStartMeeting = async (upcomingClass) => {
+
+        const userID = userData.Id;
+        const userName = userData.Name;
+
+        const joinName = `${userID} - ${userName}`;
+        const { join_url } = upcomingClass;
+
+        const joinUrlWithName = `${join_url}?uname=${encodeURIComponent(joinName)}`
+        
+        try {
+            const startWindow = window.open(joinUrlWithName, '_blank');
+
+            if (startWindow) {
+                return;
+            }
+        } catch (error) {
+            console.error('Failed to start meeting as host:', error);
+        }
+    };
 
     if (loading) {
         return <p>Loading...</p>;
@@ -107,9 +154,9 @@ function Dashboard() {
                                 <h1>Welcome Back,</h1>
                                 <h2>{userData.Name} ({userData.Id})</h2>
                             </div>
-                            <div>
+                            {/* <div>
                                 <button>Logout<span className="material-symbols-outlined">logout</span></button>
-                            </div>
+                            </div> */}
                         </div>
 
                         <div className='dashboard-top-cards'>
@@ -142,11 +189,19 @@ function Dashboard() {
                                 </ul>
                             </div>
                             <div className='dashboard-card-courses'>
-                                <h3><span className="material-symbols-outlined">school</span> Courses enrolled <span className='count'>({coursesCount}/3)</span></h3>
-                                <ul>
+                                <h3><span className="material-symbols-outlined">check</span> Attendance <span className='count'>({coursesCount}/3)</span></h3>
+                                <ul id='courses-progress'>
                                     {userData.courses.map((course, index) => (
                                         <li key={index}>
-                                            <p>{course}</p>
+                                            <div className='progress'>
+                                                <p>{course}</p>
+                                                <div className='progress-bar'>
+                                                    <div className='progress-bar-fill' style={{ width: `${userData.submissions[course] / 3 * 100}%` }}></div>
+                                                </div>
+                                                <div>
+                                                    <div><p id='progress-count'>60%</p></div>
+                                                </div>
+                                            </div>
                                         </li>
                                     ))}
                                 </ul>
@@ -154,53 +209,46 @@ function Dashboard() {
                         </div>
                         <div className='dashboard-bottom-cards'>
                             <div className='dashboard-left-cards'>
-                                <div className='dashboard-card-announcements'>
-                                    <h3><span className="material-symbols-outlined">campaign</span> Announcements</h3>
-                                    <div className='announcement'>
-                                        <div className='announcement-details'>
-                                            <span>19:48</span> <span>23.06.2024</span> <span>Admin</span>
+                            <div className='dashboard-card-announcements'>
+                                <h3><span className="material-symbols-outlined">campaign</span> Announcements</h3>
+                                {announcements.length > 0 ? (
+                                    announcements.map((announcement) => (
+                                        <div key={announcement.id} className='announcement'>
+                                            <div className='announcement-details'>
+                                                <span>{format(announcement.date, 'HH:mm')}</span><span> | </span> <span>{format(announcement.date, 'dd.MM.yyyy')}</span>
+                                            </div>
+                                            <p dangerouslySetInnerHTML={{ __html: announcement.text }}></p>
                                         </div>
-                                        <p><span>•</span> There will be a live session on graphic design on 23rd October 2024</p>
-                                    </div>
-                                    <div className='announcement'>
-                                        <div className='announcement-details'>
-                                            <span>19:48</span> <span>23.06.2024</span> <span>Admin</span>
-                                        </div>
-                                        <p><span>•</span> There will be a live session on graphic design on 23rd October 2024</p>
-                                    </div>
-                                    <div className='announcement'>
-                                        <div className='announcement-details'>
-                                            <span>19:48</span> <span>23.06.2024</span> <span>Admin</span>
-                                        </div>
-                                        <p><span>•</span> There will be a live session on graphic design on 23rd October 2024</p>
-                                    </div>
-                                    <div className='announcement'>
-                                        <div className='announcement-details'>
-                                            <span>19:48</span> <span>23.06.2024</span> <span>Admin</span>
-                                        </div>
-                                        <p><span>•</span> There will be a live session on graphic design on 23rd October 2024</p>
-                                    </div>
-                                </div>
+                                    ))
+                                ) : (
+                                    <p>No announcements available.</p>
+                                )}
+                            </div>
                             </div>
 
                             <div className='dashboard-right-cards'>
                                 <div className='dashboard-card-upcoming'>
                                     <h3><span className="material-symbols-outlined">calendar_month</span> Upcoming class</h3>
                                     {upcomingClass ? (
-                                        <div className='upcoming-class'>
-                                            <div>
-                                                <span class="material-symbols-outlined">
-                                                    videocam
-                                                </span>
-                                            </div>
-                                            <div>
-                                                <p>{upcomingClass.course}</p>
-                                                <p className='meeting-time'>{format(upcomingClass.time, 'dd MMMM   HH:mm')}</p>
-                                            </div>
+                                    upcomingClass.map((upcomingClass) => (
+                                    <div className='upcoming-class'>
+                                        <div className='class-1'>
+                                            <span className="material-symbols-outlined">videocam</span>
+                                            <p id='class-course'>{upcomingClass.course}</p>
                                         </div>
-                                    ) : (
-                                        <p>No upcoming classes.</p>
-                                    )}
+                                        <div className='class-2'>
+                                            <div>
+                                        <p className='meeting-time'>{format(upcomingClass.time, 'dd MMMM  |  HH:mm')}</p>
+                                        </div>
+                                        <div>
+                                        <button className='join-btn' onClick={() => {handleStartMeeting(upcomingClass)}}>Join</button>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    ))
+                                ) : (
+                                    <p>No upcoming classes.</p>
+                                )}
                                 </div>
                             </div>
                         </div>
