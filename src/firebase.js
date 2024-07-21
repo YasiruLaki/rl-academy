d51807ba947e7c71f54645accea2f32f8571255e
Yasiru Lakintha <96948952+YasiruLaki@users.noreply.github.com>
2024-07-21 21:16:09 +0530

    all functions done except attendancsvgsegesvse

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD-4Pr3hvDtrVmb6VujUx3r5dH2BSoXop4",
-    authDomain: "rl-academy-dffe6.firebaseapp.com",
-    projectId: "rl-academy-dffe6",
-    storageBucket: "rl-academy-dffe6.appspot.com",
-    messagingSenderId: "485840616147",
-    appId: "1:485840616147:web:639f6ec6561140703cf226"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
