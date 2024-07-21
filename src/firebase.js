9457b0deccfe0e5c05c84f83cf44762dc1cae66f
Yasiru Lakintha <96948952+YasiruLaki@users.noreply.github.com>
2024-07-22 00:08:53 +0530

    all functions done except attendance

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-console.log('Firebase API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
-console.log('Firebase Auth Domain:', process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
-console.log('Firebase Project ID:', process.env.REACT_APP_FIREBASE_PROJECT_ID);
-console.log('Firebase Storage Bucket:', process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
-console.log('Firebase Messaging Sender ID:', process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
-console.log('Firebase App ID:', process.env.REACT_APP_FIREBASE_APP_ID);
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -17,6 +11,13 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
+
+console.log('Firebase API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
+console.log('Firebase Auth Domain:', process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
+console.log('Firebase Project ID:', process.env.REACT_APP_FIREBASE_PROJECT_ID);
+console.log('Firebase Storage Bucket:', process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
+console.log('Firebase Messaging Sender ID:', process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID);
+console.log('Firebase App ID:', process.env.REACT_APP_FIREBASE_APP_ID);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
